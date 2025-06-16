e4e4b780abb87bfc9249d2282b5baebdd4466b5d
Sidekick <sidekick@side.dev>
2025-06-15 22:58:12 -0700

    Update ShowTasksAction to use ActionUpdateThread.EDT
    Modify src/main/kotlin/com/github/sidedev/sidekick/actions/ShowTasksAction.kt to override getActionUpdateThread() to return ActionUpdateThread.EDT and add the required import.

--- src/main/kotlin/com/github/sidedev/sidekick/actions/ShowTasksAction.kt
+++ src/main/kotlin/com/github/sidedev/sidekick/actions/ShowTasksAction.kt
@@ -1,6 +1,7 @@
 package com.github.sidedev.sidekick.actions
 
 import com.github.sidedev.sidekick.toolWindow.SidekickToolWindowManager
+import com.intellij.openapi.actionSystem.ActionUpdateThread
 import com.intellij.openapi.actionSystem.AnAction
 import com.intellij.openapi.actionSystem.AnActionEvent
 import com.intellij.icons.AllIcons
@@ -20,5 +21,9 @@ class ShowTasksAction : AnAction(
     override fun update(e: AnActionEvent) {
         // Action is enabled when there's a project
         e.presentation.isEnabled = e.project != null
+    }
+
+    override fun getActionUpdateThread(): ActionUpdateThread {
+        return ActionUpdateThread.EDT
     }
 }
